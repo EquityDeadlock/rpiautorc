66cc0392806218c3c52f78d03d60c1585ba77a2a
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-17 16:43:52 -0400

    Created class Database
    This is how all interactions with the database will be done.
    Current implementations allow for the switching of reference point and
    pushing to the Real-time Database

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
-import firebase_admin
-from firebase_admin import credentials
+import firebase_admin, json
+from firebase_admin import credentials, db
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+class Database:
+    def __init__(self):        
+        self.__cred = firebase_admin.credentials.Certificate('serviceAccountKey.json')
+        self.__default_app = firebase_admin.initialize_app(self.__cred, {"databaseURL":"https://autonomous-rc-default-rtdb.firebaseio.com/"})
+    
+    def setReference(self, reference: str) -> str:
+        self.__ref = db.reference(f"/{reference}")
+
+    def push(self, data: json):
+        self.__ref.push(data)
 
 
