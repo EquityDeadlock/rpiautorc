03f2a9574584ca1a691b7cabd7bb8c55b88c067e
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-17 23:11:51 -0400

    Example testing code added in main()

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from sensors import *
+from database import *
 
 def main():
-    pass
+    fireDB = Database()
+    fireDB.setReference("Accelerometer")
+    
+    accel = Accelerometer()
+    fireDB.set(accel.getData())
+
 
 if __name__ == "__main__":
     main()
