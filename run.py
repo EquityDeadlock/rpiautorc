8e9abb18b24a5e08827d9b0687540c2df2dc7a3d
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-18 01:15:26 -0400

    Added simulated value generator
    Sensors can now use a psuedo-random number generator to generate values
    for testing purposes
    
    All sensors can be simulated using Sensor.SIMULATION = True
    Sensors can be toggled per-case using sim=True in the constructor

--- run.py
+++ run.py
@@ -1,12 +1,27 @@
 from sensors import *
 from database import *
 
+
+fireDB = Database() # Database Object
+Sensor.SIMULATE = False # Toggle all Sensors to simulated values
+
+# Sensor Objects
+ultra = Ultrasonic()
+accel = Accelerometer()
+motor = MotorController()
+
 def main():
-    fireDB = Database()
-    fireDB.setReference("Accelerometer")
-    
-    accel = Accelerometer()
+    while True:
+        writeUltra()
+        writeAccel()
+
+def writeUltra(): 
+    fireDB.setReference(Database.ULTRASONIC)
+    fireDB.set(ultra.getData())
+def writeAccel(): 
+    fireDB.setReference(Database.ACCELEROMETER)
     fireDB.set(accel.getData())
+
 
 
 if __name__ == "__main__":
--- sensors.py
+++ sensors.py
@@ -1,34 +1,68 @@
 from abc import ABC, abstractmethod
-import json, subprocess
+import json, subprocess, random
 class Sensor(ABC):
-    def __init__(self):
+    SIMULATE = False
+    def __init__(self, sim=False):
         self.__data: str = None
-        self.__prev_data = None
+        self.__sim = sim
+        
+    def _isSimulation(self) -> bool:
+        return Sensor.SIMULATE or self.__sim
 
-    @abstractmethod
-    def getData(self) -> json:
-        pass
-
+    def _getRandomFloat(self, minimum: float, maximum: float, decimalPlaces: int) -> float:
+        return round(random.uniform(minimum, maximum), decimalPlaces)
+    
 class Ultrasonic(Sensor):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, sim=False):
+        super().__init__(sim)
+
+        if super()._isSimulation():
+            self.__minDistance = 2.0
+            self.__maxDistance = 30.0
 
     def getData(self) -> json:
-        return ""
+        if super()._isSimulation():
+            return {"distance":super()._getRandomFloat(2.0, 30.0, 2)}
+        else:
+            return {"distance":0.0}
+
+
 
 class MotorController(Sensor):
+    def __init__(self, sim=False):
+        super().__init__(sim)
     def getData(self) -> json:
         return ""
 
 class Accelerometer(Sensor):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, sim=False):
+        super().__init__(sim)
+        
+        if super()._isSimulation():
+            self.__minTime: float = 0.0
+            self.__maxTime: float = 1
+            self.__minX: float = -10.0
+            self.__maxX: float = 10.0
+            self.__minY: float = -10.0
+            self.__maxY: float = 10.0
+            self.__minZ: float = -2.0
+            self.__maxZ: float = 2.0
+            self.__places: int = 1
 
     def __readData(self):
         self.__data = subprocess.Popen("sudo ./adx -f 1 -t 1 | head -n 1", shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8").strip()
+    
     def __delimitData(self) -> [str]:
         return self.__data.split(",")
+    
     def getData(self) -> json:
-        self.__readData()
-        fields = self.__delimitData()
-        return {"time":fields[0],"x":fields[1], "y":fields[2], "z":fields[3]}
+        if super()._isSimulation():
+            time = super()._getRandomFloat(self.__minTime, self.__maxTime, self.__places)
+            x = super()._getRandomFloat(self.__minX, self.__maxX, self.__places)
+            y = super()._getRandomFloat(self.__minY, self.__maxY, self.__places)
+            z = super()._getRandomFloat(self.__minZ, self.__maxZ, self.__places)
+            return {"time":time,"x":x, "y":y, "z":z}
+        else:
+            self.__readData()
+            fields = self.__delimitData()
+            return {"time":fields[0],"x":fields[1], "y":fields[2], "z":fields[3]}
