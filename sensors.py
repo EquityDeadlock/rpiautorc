38c1a5a634d4b6e33cf8aaa254f295635aeab047
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-17 22:40:51 -0400

    Implemented Accelerometer
    Reads from sensor, parses the data and can return data in json format

--- sensors.py
+++ sensors.py
@@ -1,16 +1,34 @@
 from abc import ABC, abstractmethod
+import json, subprocess
 class Sensor(ABC):
+    def __init__(self):
+        self.__data: str = None
+        self.__prev_data = None
+
     @abstractmethod
-    def readData() -> str:
+    def getData(self) -> json:
         pass
 
 class Ultrasonic(Sensor):
-    def readData() -> str:
-        pass
+    def __init__(self):
+        super().__init__()
+
+    def getData(self) -> json:
+        return ""
+
 class MotorController(Sensor):
-    def readData() -> str:
-        pass
-    
+    def getData(self) -> json:
+        return ""
+
 class Accelerometer(Sensor):
-    def readData() -> str:
-        pass
+    def __init__(self):
+        super().__init__()
+
+    def __readData(self):
+        self.__data = subprocess.Popen("sudo ./adx -f 1 -t 1 | head -n 1", shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8").strip()
+    def __delimitData(self) -> [str]:
+        return self.__data.split(",")
+    def getData(self) -> json:
+        self.__readData()
+        fields = self.__delimitData()
+        return {"time":fields[0],"x":fields[1], "y":fields[2], "z":fields[3]}
