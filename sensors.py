60bf0779b3aa2de5c58af68cc7111dc34256ce19
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-18 15:27:44 -0400

    Added get()

--- sensors.py
+++ sensors.py
@@ -1,6 +1,5 @@
-from abc import ABC, abstractmethod
 import json, subprocess, random
-class Sensor(ABC):
+class Sensor:
     SIMULATE = False
     def __init__(self, sim=False):
         self.__data: str = None
@@ -38,16 +37,15 @@ class Accelerometer(Sensor):
     def __init__(self, sim=False):
         super().__init__(sim)
         
-        if super()._isSimulation():
-            self.__minTime: float = 0.0
-            self.__maxTime: float = 1
-            self.__minX: float = -10.0
-            self.__maxX: float = 10.0
-            self.__minY: float = -10.0
-            self.__maxY: float = 10.0
-            self.__minZ: float = -2.0
-            self.__maxZ: float = 2.0
-            self.__places: int = 1
+        self.__minTime: float = 0.0
+        self.__maxTime: float = 1
+        self.__minX: float = -10.0
+        self.__maxX: float = 10.0
+        self.__minY: float = -10.0
+        self.__maxY: float = 10.0
+        self.__minZ: float = -2.0
+        self.__maxZ: float = 2.0
+        self.__places: int = 1
 
     def __readData(self):
         self.__data = subprocess.Popen("sudo ./adx -f 1 -t 1 | head -n 1", shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8").strip()
@@ -65,4 +63,4 @@ class Accelerometer(Sensor):
         else:
             self.__readData()
             fields = self.__delimitData()
-            return {"time":fields[0],"x":fields[1], "y":fields[2], "z":fields[3]}
+            return {"time":float(fields[0]),"x":float(fields[1]), "y":float(fields[2]), "z":float(fields[3])}
