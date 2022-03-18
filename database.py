ddb376a635be92f264e4d3efb4523fd565e0fb53
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2022-03-17 23:10:37 -0400

    Added set()
    Directly updates values for chosesn reference

--- database.py
+++ database.py
@@ -3,13 +3,23 @@ from firebase_admin import credentials, db
 
 class Database:
     def __init__(self):        
-        self.__cred = firebase_admin.credentials.Certificate('serviceAccountKey.json')
+        self.__cred = firebase_admin.credentials.Certificate("serviceAccountKey.json")
         self.__default_app = firebase_admin.initialize_app(self.__cred, {"databaseURL":"https://autonomous-rc-default-rtdb.firebaseio.com/"})
     
-    def setReference(self, reference: str) -> str:
+    def setReference(self, reference: str):
         self.__ref = db.reference(f"/{reference}")
+
+    def getReference(self, reference: str):
+        return self.__ref.get()
 
     def push(self, data: json):
         self.__ref.push(data)
+    
+    def set(self, data: json):
+        self.__ref.set(data)
 
-
+    def update(self, findPair: json):
+        temp_ref = self.__ref.get()
+        #for key, value in temp_ref.items():
+            #if(value
+             #   self.__ref.child(key).update(newPair)
